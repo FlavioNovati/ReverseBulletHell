813fd46d344e3a27124c16d27e7148c51569cd1f
FlavioNovati <f.novati10@gmail.com>
2024-12-05 18:20:58 +0100

    Update

--- Assets/Code/Scripts/EntityScript/View/Entity_View.cs
+++ Assets/Code/Scripts/EntityScript/View/Entity_View.cs
@@ -13,8 +13,7 @@ namespace Entity_System.Entity
         public void SetPosition(Vector2 position) => transform.position = position;
         public void SetLookDirection(Vector2 lookDir)
         {
-            _spriteRenderer.transform.rotation = Quaternion.LookRotation( new Vector3(lookDir.x, lookDir.y, 0f));
-            _spriteRenderer.transform.rotation *= new Quaternion(90f, 0f, 0f, 0f);
+
         }
 
         public void SetHealth(float percentage) => _healthBar.Progress = percentage;
